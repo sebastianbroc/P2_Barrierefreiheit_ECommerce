bc3f8ea4a96498a333aec12880b49a5f6a156d24
Taha <tsmoujtahid@outlook.de>
2023-05-30 18:41:04 +0200

    added coments and guidelines

---
+++ GuidelineApi/Services/BaseService.cs
@@ -0,0 +1,52 @@
+﻿using GuidelineAPI;
+
+namespace UserAccessService.Service;
+
+public class BaseService<T> : IBaseService<T> where T : BaseModel
+{
+
+    public DBContext _context;
+    
+    public BaseService(DBContext context)
+    {
+        _context = context;
+    }
+
+    public T? Get(T obj)
+    {
+        return _context.Set<T>().Find(obj);
+    }
+
+    public List<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+    
+    public T Create(T obj)
+    {
+        _context.Set<T>().Add(obj);
+        _context.SaveChanges();
+        return obj;
+    }
+
+    public T Update(T obj)
+    {
+        _context.Set<T>().Update(obj);
+        _context.SaveChanges();
+        return obj;
+    }
+
+    public bool Delete(Guid id)
+    {
+        var toDelete = _context.Set<T>().FirstOrDefault(t => t.id == id);
+        if (toDelete == null)
+        {
+            return false;
+        }
+        _context.Set<T>().Remove(toDelete);
+        _context.SaveChanges();
+        return true;
+    }
+
+    public IServiceProvider ServiceProvider { get; }
+}
---
+++ GuidelineApi/Services/ICommentService.cs
@@ -0,0 +1,8 @@
+﻿using UserAccessService.Service;
+
+namespace GuidelineAPI.Services;
+
+public interface ICommentService : IBaseService<Comment>
+{
+    
+}
