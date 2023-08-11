9e1d38d919082260eba0ac37598482a9b43b879e
Sebastian Brock <sebastian.brock@outlook.de>
2023-08-11 09:04:22 +0200

    remove accidentally committed files

--- GuidelineApi/JWTSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace GuidelineAPI {
-  public class JWTSettings
-  {
-    public string SecretKey { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
-  }
-}
